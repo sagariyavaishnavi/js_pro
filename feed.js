87d83f402feca62c1165d41cbdb17cfe608a06d3
sagariyavaishnavi <sagariyavaishnavi@gmail.com>
2024-12-17 15:12:23 +0530

    change in file

--- feed.js
+++ feed.js
@@ -3,15 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
         let name = document.getElementById("myName").value;
         let email = document.getElementById("myEmail").value;
         let division = document.getElementById("myDivision").value;
-        let comment = document.getElementById("mycom").value;  
+        let comment = document.getElementById("mycom").value;
 
-        console.log({ name, email, division, comment });
-        document.getElementById("output").textContent = 
+       
+        console.log("Name:", name);
+        console.log("Email:", email);
+        console.log("Division:", division);
+        console.log("Comment:", comment);
+
+        document.getElementById("output").textContent =
             `Thank you, ${name}! Your feedback is submitted!`;
     }
 
     document.getElementById("mybutton").addEventListener("click", function(e) {
         e.preventDefault();  
+        console.log("Button clicked!");
         show();
     });
 });
